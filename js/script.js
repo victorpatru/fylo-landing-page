7429413c391ed7db72026950146147fbf955b1bc
Victor Patru <patruvictor.mihai@gmail.com>
2022-08-17 18:30:38 +0300

    finished project

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// Getting our items from the DOM
+const themeToggleBtn = document.getElementById("theme-toggle");
+const themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+const themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
+
+// Displaying the correct toggle button in the DOM
+if (
+  localStorage.getItem("color-theme") === "dark" ||
+  (!("color-theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  themeToggleLightIcon.classList.remove("hidden");
+} else {
+  themeToggleDarkIcon.classList.remove("hidden");
+}
+
+// Event listener
+themeToggleBtn.addEventListener("click", () => {
+  // Change the buttons displayed to the user
+  themeToggleLightIcon.classList.toggle("hidden");
+  themeToggleDarkIcon.classList.toggle("hidden");
+
+  // Check for local storage
+  if (localStorage.getItem("color-theme")) {
+    // If light, make dark and save to local storage
+    if (localStorage.getItem("color-theme") === "light") {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
+    }
+  } else {
+    // If not in local storage
+    if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
+    }
+  }
+});
